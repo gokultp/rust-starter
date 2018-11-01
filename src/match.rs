3d46f0e39b6d8579eb58c70bf6590edb16bfa646
Gokul TP <tp.gokul@gmail.com>
2018-11-01 11:18:18 +0000

    took matcher fn to main file

--- src/match.rs
+++ src/match.rs
@@ -1,13 +1,4 @@
-fn testMatch(tshirt_size){
-    let tshirt_size = match tshirt_width {
-        16 => "S", // check 16
-        17 | 18 => "M", // check 17 and 18
-        19 ... 21 => "L", // check from 19 to 21 (19,20,21)
-        22 => "XL",
-        _ => "Not Available",
-    };
 
-}
 
 
 
