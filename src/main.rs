2d67cfe68a79f28c6705b0cb505064acd4c6cdb4
Gokul TP <tp.gokul@gmail.com>
2018-11-02 11:18:18 +0000

    took matcher fn to main file

--- src/main.rs
+++ src/main.rs
@@ -11,3 +11,13 @@ fn fact( n: i32) -> i32 {
         n * fact(n -1)
     }
 }
+fn testMatch(tshirt_size){
+    let tshirt_size = match tshirt_width {
+        16 => "S", // check 16
+        17 | 18 => "M", // check 17 and 18
+        19 ... 21 => "L", // check from 19 to 21 (19,20,21)
+        22 => "XL",
+        _ => "Not Available",
+    };
+
+}
